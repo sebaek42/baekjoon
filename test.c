26d0ef5fde519ac936babad3fea090aaa9c25d91
sebaek42 <sebaek@student.42seoul.kr>
2020-10-07 11:52:14 +0900

    10/7

---
+++ test.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+int  main()
+{
+	int A;
+	int B;
+	int C;
+
+	while (1)
+	{
+	scanf("%d %d %d", &A, &B, &C);
+	int min;
+	int mid;
+	int max;
+		if (A < B)
+	{
+		min = A;
+		max = B;
+	}
+	else
+	{
+		min = B;
+		max = A;
+	}
+
+	if (max < C)
+	{
+		mid = max;
+		max = C;
+	}
+	else
+	{
+		if (min > C)
+		{
+			mid = min;
+			min = C;
+		}
+		else
+		{
+			mid = C;
+		}
+
+	}
+
+
+	printf("%d %d %d\n", min, mid, max);
+
+	}
+}
